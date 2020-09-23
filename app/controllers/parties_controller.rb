67fe1f01da8fe00dc9e5eb1d7dae524ca4323a85
Annabel Wilmerding <awilmerding@AnnabelngsMBPro.cable.rcn.com>
2020-09-23 13:33:39 -0400

    base code

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -8,22 +8,15 @@ class PartiesController < ApplicationController
   end
 
   def new
-    @party = Party.new
-    @party.snacks.build
-    @party.snacks.build
-    @party.snacks.build
+
+
   end
 
   def create
-    party = Party.create(party_params)
-    redirect_to party_path(party)
+
   end
 
 
-  private
 
-  def party_params
-    params.require(:party).permit(:name, :date, :budget, snacks_attributes: [:name, :description])
-  end
 
 end
